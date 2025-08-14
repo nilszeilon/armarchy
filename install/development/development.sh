55f825dfe52f37fe13185ca33d60fc2850816501
Nils Barr Zeilon <nils.barr.zeilon@gmail.com>
2025-08-14 22:19:34 +0200

    install lazydocker instead of the bin (x86-64 arch only)

--- install/development/development.sh
+++ install/development/development.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed \
-  cargo clang llvm mise \
-  imagemagick \
-  mariadb-libs postgresql-libs \
-  github-cli \
-  lazygit lazydocker-bin
+if [ -z "$OMARCHY_ARM" ]; then
+  yay -S --noconfirm --needed \
+    cargo clang llvm mise \
+    imagemagick \
+    mariadb-libs postgresql-libs \
+    github-cli \
+    lazygit lazydocker-bin
+else
+  yay -S --noconfirm --needed \
+    cargo clang llvm mise \
+    imagemagick \
+    mariadb-libs postgresql-libs \
+    github-cli \
+    lazygit lazydocker
+fi
