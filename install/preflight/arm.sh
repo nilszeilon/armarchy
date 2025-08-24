7b08c504fc462e40632368dc23521bc2cddd9bc5
Nils Barr Zeilon <nils.barr.zeilon@gmail.com>
2025-08-24 22:38:23 +0000

    override conf files with symlink

--- install/preflight/arm.sh
+++ install/preflight/arm.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Auto-detect ARM architecture and set flags
 arch=$(uname -m)
 if [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
@@ -9,9 +10,7 @@ if [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
   mkdir -p ~/.local/state/omarchy
   touch ~/.local/state/omarchy/arm.mode
 
-  # make use of the right shaders for walker to work correctly
-  echo "env = VK_ICD_FILENAMES,/usr/share/vulkan/icd.d/lvp_icd.aarch64.json" >>~/.config/hypr/envs.conf
-
-  # ARM/aarch64 architecture needs its own config file for excluded apps
+  # ARM/aarch64 architecture override conf files
+  ln -sf ~/.local/share/omarchy/default/hypr/aarch64_envs.conf ~/.local/share/omarchy/default/hypr/envs.conf
   ln -sf ~/.local/share/omarchy/default/hypr/aarch64_apps.conf ~/.local/share/omarchy/default/hypr/apps.conf
 fi
