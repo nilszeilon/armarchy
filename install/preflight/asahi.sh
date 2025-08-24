9f1829b2682aa0365aae7bd3090e8df1e114b71f
Nils Barr Zeilon <nils.barr.zeilon@gmail.com>
2025-08-24 15:45:15 +0200

    add bash

--- install/preflight/asahi.sh
+++ install/preflight/asahi.sh
@@ -19,7 +19,7 @@ if [ "$EUID" -eq 0 ] && [ "$(uname -m)" = "aarch64" ]; then
     pacman -Q asahi-scripts &>/dev/null; then
     echo "Detected Asahi Linux - running initial setup..."
     # using exec instead of source to continue the installation after switching user
-    exec $OMARCHY_INSTALL/preflight/asahi.sh
+    exec bash $OMARCHY_INSTALL/preflight/asahi.sh
 
     set -e
 
