c2d08d3bd195a990e4da7e8e68b6a7fc8687d2cf
Nils Barr Zeilon <nils.barr.zeilon@gmail.com>
2025-08-24 18:57:30 +0200

    add noconfirm on pipewire things

--- install/desktop/audio.sh
+++ install/desktop/audio.sh
@@ -2,5 +2,5 @@
 
 # on macbook m-series we need pipewire
 if [ -n "$OMARCHY_ARM" ]; then
-  yay -S --needed pipewire-alsa pipewire-pulse wireplumber pipewire-jack asahi-audio
+  yay -S --needed --noconfirm pipewire-alsa pipewire-pulse wireplumber pipewire-jack asahi-audio
 fi
