a5815455c8e8336147c59f05a3bf0560af67538e
Nils Barr Zeilon <nils.barr.zeilon@gmail.com>
2025-08-29 12:36:31 +0200

    add more mirrors

--- install/preflight/repositories.sh
+++ install/preflight/repositories.sh
@@ -23,6 +23,8 @@ sudo tee /etc/pacman.d/mirrorlist >/dev/null <<'EOF'
 Server = https://mirror.omarchy.org/$repo/os/$arch
 Server = https://mirror.rackspace.com/archlinux/$repo/os/$arch
 Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch
+Server = https://mirrors.kernel.org/archlinux/$repo/os/$arch
+Server = https://mirror.f4st.host/archlinux/$repo/os/$arch
 Server = https://archlinuxarm.org/$arch/$repo
 EOF
 
